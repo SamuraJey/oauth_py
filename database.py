5db3434863d74fbdfa99c8edcc9814de2dd01ceb
SamuraJey <cergeypro703@gmail.com>
2025-01-19 22:06:48 +0500

    123

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from typing import cast
+from flask import current_app
 import pycouchdb
 
 from dotenv_load import SiteSettings
+
 
 def get_url(settings: SiteSettings) -> str:
     login = settings.couchdb_user
@@ -10,9 +12,31 @@ def get_url(settings: SiteSettings) -> str:
     port = settings.couchdb_port
     return f'http://{login}:{password}@{url}:{port}'
 
+# curl -X PUT http://adm:pass@127.0.0.1:5984/_users
+
+# curl -X PUT http://adm:pass@127.0.0.1:5984/_replicator
+
+
+def init_db(couch_db_server: pycouchdb.Server) -> None:
+
+    try:
+        couch_db_server.create('_users')
+    except pycouchdb.exceptions.Conflict:
+        current_app.logger.info('Database _users already exists')
+        pass
+
+    try:
+        couch_db_server.create('_replicator')
+    except pycouchdb.exceptions.Conflict:
+        current_app.logger.info('Database _replicator already exists')
+        pass
+
+
 def get_db(settings: SiteSettings, database_name: str) -> pycouchdb.client.Database:
     full_url = get_url(settings)
     couchdb_server = pycouchdb.Server(full_url)
+    init_db(couchdb_server)
+    db = None
     try:
         db = couchdb_server.database(database_name)
     except pycouchdb.exceptions.NotFound:
